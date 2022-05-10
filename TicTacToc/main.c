500f784d69216b818c78b6880b60912091869a8c
sanskar sahu <sahup3296@gmail.com>
2022-05-10 21:35:14 +0530

    almost done

--- TicTacToc/main.c
+++ TicTacToc/main.c
@@ -5,31 +5,21 @@
 #include <time.h>
 #include <string.h>
 
-// Font code colours
-#define RED "\x1B[31m"
+//* Font code colours
 #define GRN "\x1B[32m"
 #define YEL "\x1B[33m"
 #define BLU "\x1B[34m"
-#define MAG "\x1B[35m"
 #define CYN "\x1B[36m"
-#define WHT "\x1B[37m"
-// back ground code colours
+
+//* back ground code colours
 #define gRED "\x1B[41m"
-#define gGRN "\x1B[42m"
-#define gYEL "\x1B[43m"
-#define gBLU "\x1B[44m"
-#define gMAG "\x1B[45m"
-#define gCYN "\x1B[46m"
-#define gWHT "\x1B[47m"
 
 //* TYPOGRAPHY
 #define Strikethrough "\x1B[9m"
 #define Underlined "\x1B[4m"
-#define Italic "\x1B[3m"
-#define Faint "\x1B[2m"
 #define BOLD "\x1B[1m"
 
-// reset foreground and background to normal colours
+//* reset foreground and background to normal colours
 #define RESET "\x1B[0m"
 
 // TODO ALL MAIN VARIBLES
@@ -51,7 +41,7 @@ int winState[9][3] = {
 
 //! ALL MAIN FUNCTIONS SEE DOWN BELOW
 void drew(void);
-int field(void);
+int checkField(void);
 void resetArea(void);
 void checkWinner(char *);
 void userInput(void);
@@ -79,6 +69,7 @@ void validate()
         {
             if (gameArea[winState[i][0]] == mainPlayer && gameArea[winState[i][1]] == mainPlayer && gameArea[winState[i][2]] == mainPlayer)
             {
+                printf("%s", gameArea[i]);
                 winner = mainPlayer;
             }
             if (gameArea[winState[i][0]] == computer && gameArea[winState[i][1]] == computer && gameArea[winState[i][2]] == computer)
@@ -112,7 +103,7 @@ void resetArea()
 {
     int i, j;
     for (i = 0; i < 9; i++)
-        gameArea[i] = "\t";
+        gameArea[i] = checkSpace;
 }
 
 //? CHECK ALL field FILLED OR NOT
