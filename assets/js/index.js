6ca9b2ca0043c613d1bfa3a92dadeccd3ffff3c5
DSDmark <sahup3296@gmail.com>
2023-01-13 00:36:42 +0630

    UPDATE

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,11 @@
 const canvas = document.getElementById('cvs')
-const stopButton = document.getElementById('stopbutton')
+const toggleButton = document.getElementById('toggleButton')
+const speedOfSwaping = document.getElementById('speedofswaping')
+const sizeofcanvas = document.getElementById('sizeofcanvas')
+const numsoflines = document.getElementById('numberoflines')
+const sortedlinecolor = document.getElementById('sortedlinecolor')
+const linescolors = document.getElementById('linescolors')
+
 const ctx = canvas.getContext('2d')
 const CH = (canvas.height = 300)
 const CW = (canvas.width = 600)
@@ -9,10 +15,13 @@ const BubbleSort = {
   i: 0,
   j: 0,
   seedValue: [CH],
-  loopSpeed: 100,
+  loopSpeed: 200,
+  clear: null,
   isLoop: true,
   borderStyle: { size: 1, color: 'white', style: 'solid' },
   color: 'white',
+  linesColor: 'white',
+  sortingLineColor: 'red',
   sortedLine: null,
   draw() {
     canvas.style.border = `${this.borderStyle.size}px ${this.borderStyle.style} ${this.borderStyle.color}`
@@ -20,44 +29,43 @@ const BubbleSort = {
     const lineSpacing = 10
     ctx.lineWidth = 5
     for (let j = 1; j < this.seedValue.length; j++) {
-          
-    for (let j = 0; j < this.seedValue.length; j++) {
-      const y = j * lineSpacing
-      ctx.beginPath()
-      ctx.moveTo(y, CH)
-      ctx.lineTo(y, CH - this.seedValue[j])
-      ctx.closePath()
-      if (this.sortedLine === this.seedValue[j]) {
-        this.color = 'red'
-        ctx.strokeStyle = this.color
-        ctx.stroke()
-      } else {
-        this.color = 'white'
-        ctx.strokeStyle = this.color
-        ctx.stroke()
+      for (let j = 0; j < this.seedValue.length; j++) {
+        const y = j * lineSpacing
+        ctx.beginPath()
+        ctx.moveTo(y, CH)
+        ctx.lineTo(y, CH - this.seedValue[j])
+        ctx.closePath()
+        if (this.sortedLine === this.seedValue[j]) {
+          this.color = this.sortingLineColor
+          ctx.strokeStyle = this.color
+          ctx.stroke()
+        } else {
+          this.color = this.linesColor
+          ctx.strokeStyle = this.color
+          ctx.stroke()
+        }
       }
     }
-  }
-},
-  update() {
+  },
 
+  update() {
     if (this.seedValue[this.j] > this.seedValue[this.j + 1]) {
       ;[this.seedValue[this.j], this.seedValue[this.j + 1]] = [
         this.seedValue[this.j + 1],
         this.seedValue[this.j],
       ]
+
       this.sortedLine = this.seedValue[this.j + 1]
     }
-    if (this.i < this.seedValue.length || isClick) {
+    if (this.i < this.seedValue.length) {
       this.j++
       if (this.j >= this.seedValue.length - this.i - 1) {
-        this.color = 'red'
         this.j = 0
         this.i = this.i + 1
       }
     } else {
       this.isLoop = false
-      clearInterval()
+      clearInterval(this.clear)
     }
 
     this.draw()
@@ -69,30 +77,72 @@ const BubbleSort = {
       this.seedValue[this.i] = Math.random() * (CH - 0 + 1) + 0
     }
     this.i = 0
-
-    // Hendling Evnets.
   },
-  stopLoop() {
-          clearInterval();
-           },
+
+  speedControl(e) {
+    this.stopInterval()
+    this.isLoop = false
+    this.loopSpeed = e
+    this.startInterval()
+    this.isLoop = true
+  },
+  stopInterval() {
+    clearInterval(this.clear)
+  },
+  startInterval() {
+    // You could use window.requestAnimationFrame as well. but, for simplicity i'm using setInterval.
+    this.clear = setInterval(() => {
+      this.update(this.seedValue)
+    }, this.loopSpeed)
+  },
+  linescolorsChange(color, lineType = false) {
+    this.stopInterval()
+    lineType ? (this.sortingLineColor = color) : (this.linesColor = color)
+    this.startInterval()
+  },
 }
 
-// You could use window.requestAnimationFrame as well. but, for simplicity i'm using setInterval.
-        let cleaer;
-   stopButton.addEventListener('click',()=>{
-         clearInterval(cleaer);
-   }   )
+// Hendling the events
+sortedlinecolor.addEventListener('focusout', (e) => {
+  let colorValue = e.target.value
+  colorValue = colorValue
+    .toLowerCase()
+    .toString()
+    .replace(/[^a-z]/g, '')
+  if (!(colorValue === '')) {
+    BubbleSort.linescolorsChange(colorValue, true)
+  }
+})
+
+linescolors.addEventListener('focusout', (e) => {
+  let colorValue = e.target.value
+  colorValue = colorValue
+    .toLowerCase()
+    .toString()
+    .replace(/[^a-z]/g, '')
+  if (!(colorValue === '')) {
+    BubbleSort.linescolorsChange(colorValue)
+  }
+})
+
+toggleButton.addEventListener('click', () => {
+  if (BubbleSort.isLoop) {
+    BubbleSort.stopInterval()
+    BubbleSort.isLoop = false
+  } else {
+    BubbleSort.startInterval()
+    BubbleSort.isLoop = true
+  }
+})
+
+speedOfSwaping.addEventListener('change', (e) => {
+  document.getElementById('showValue').innerText = e.target.value
+  BubbleSort.speedControl(e.target.value)
+})
+
 function render() {
   BubbleSort.startup()
-             if (BubbleSort.isLoop) { 
-
-    cleaer = setInterval(() => {
-      BubbleSort.update(BubbleSort.seedValue)
-             
-    }, BubbleSort.loopSpeed)
-                    }else{
-                     console.log("final");
-             } 
+  BubbleSort.startInterval()
 }
 
 render() // calling the main functions.
